1982a88319506e0621e906246d625e758254df2a
Dhriti <dhriti.baruah20@gmail.com>
2024-12-28 12:46:51 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { TouchableOpacity } from "react-native";
+import Notes from "./components/Notes/NotesScreen";
+import AddNote from "./components/Notes/AddNote";
+import ViewNote from "./components/Notes/ViewNote";
 
-export default function App() {
+function HomeScreen({ navigation }) {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+     <TouchableOpacity
+       style={{
+         width: "80%",
+         padding: 5,
+         borderColor: "red",
+         borderWidth: 1,
+         borderRadius: 15,
+         marginVertical: 10
+       }}
+     >
+       <Text
+         style={{ textAlign: "center", color: "red" }}
+         onPress={() =>
+           navigation.navigate("NotesScreen")
+         }
+       >
+         Notes
+       </Text>
+     </TouchableOpacity>
+   </View>
+ );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Home" component={HomeScreen} />
+       <Stack.Screen name="NotesScreen" component={Notes} />
+       <Stack.Screen name="AddNote" component={AddNote}/>
+       <Stack.Screen name="ViewNote" component={ViewNote}/>
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
+}
+
+export default App;
+
+
+
